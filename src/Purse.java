1ef7310ac782b799dc058cf981b763bf7f8ac4cb
kend5l <kendalkyle50@gmail.com>
2024-08-31 16:46:39 -0500

    register built

--- src/Purse.java
+++ src/Purse.java
@@ -3,24 +3,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Purse {
-    private final Map<Denomination.Denominations, Integer> cash;
+    private final Map<DenominationList.Denominations, Integer> cash;
 
     public Purse() {
         cash = new HashMap<>();
     }
 
-    public void add(Denomination.Denominations type, int num) {
+    public void add(DenominationList.Denominations type, int num) {
         cash.put(type, cash.getOrDefault(type, 0) + num);
     }
 
-    public double remove(Denomination.Denominations type, int num) {
+    public double remove(DenominationList.Denominations type, int num) {
         cash.put(type, cash.get(type) - num);
         return type.amount() * num;
     }
 
     public double getValue() {
         double total = 0;
-        for (Map.Entry<Denomination.Denominations, Integer> entry : cash.entrySet()) {
+        for (Map.Entry<DenominationList.Denominations, Integer> entry : cash.entrySet()) {
             total += entry.getKey().amount() * entry.getValue();
         }
         return total;
@@ -28,7 +28,7 @@ public class Purse {
 
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        for (Map.Entry<Denomination.Denominations, Integer> entry : cash.entrySet()) {
+        for (Map.Entry<DenominationList.Denominations, Integer> entry : cash.entrySet()) {
             sb.append(entry.getKey().name()).append(": ").append(entry.getValue()).append("\n");
         }
         return sb.toString();
--- src/Register.java
+++ src/Register.java
@@ -1,41 +1,85 @@
 import java.util.Scanner;
 
+
 public class Register {
 
-        // method responsible for calculation
+
+    // method responsible for calculation
         public static Purse makeChange(double amount) {
+
             Purse purse = new Purse();
+
             // convert dollars to cents
             int cents = (int) (amount * 100);
+
             // num of hundreds
             int hundreds = cents / 10000;
-            cents %= 10000;
+            if (hundreds > 0){
+                purse.add(DenominationList.hundredNote, hundreds);
+                cents %= 10000;
+            }
+
             // num of fifties
             int fifties = cents / 5000;
-            cents %= 5000;
+            if (fifties > 0){
+                purse.add(DenominationList.fiftyNote, fifties);
+                cents %= 5000;
+            }
+
             // num of twenties
             int twenties = cents / 2000;
-            cents %= 2000;
+            if (twenties > 0){
+                purse.add(DenominationList.twentyNote, twenties);
+                cents %= 2000;
+            }
+
             // num of tens
             int tens = cents / 1000;
-            cents %= 1000;
+            if (tens > 0){
+                purse.add(DenominationList.tenNote, tens);
+                cents %= 1000;
+            }
+
             // num of fives
             int fives = cents / 500;
-            cents %= 500;
+            if (fives > 0){
+                purse.add(DenominationList.fiveNote, fives);
+                cents %= 500;
+            }
+
             // num of ones
             int ones = cents / 100;
-            cents %= 100;
+            if (ones > 0){
+                purse.add(DenominationList.oneNote, ones);
+                cents %= 100;
+            }
+
             // num of quarters
             int quarters = cents / 25;
-            cents %= 25;
+            if (quarters > 0){
+                purse.add(DenominationList.quarter, quarters);
+                cents %= 25;
+            }
+
             // num of dimes
             int dimes = cents / 10;
-            cents %= 10;
+            if (dimes > 0){
+                purse.add(DenominationList.dime, dimes);
+                cents %= 10;
+            }
+
             // num of nickels
             int nickels = cents / 5;
-            cents %= 5;
+            if (nickels > 0){
+                purse.add(DenominationList.nickel, nickels);
+                cents %= 5;
+            }
+
             // num of pennies
             int pennies = cents;
+            if (pennies > 0){
+                purse.add(DenominationList.penny, pennies);
+            }
 
             return purse;
         }
@@ -46,6 +90,9 @@ public class Register {
             double amount = sc.nextDouble();
             // method call
             Purse purse = makeChange(amount);
+            System.out.println("Change Given: ");
+            System.out.println(purse.toString());
+            sc.close();
         }
 }
 
