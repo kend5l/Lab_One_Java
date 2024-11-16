c43f8c1ac00f036b15219e9ac7011246b60622f6
kend5l <kendalkyle50@gmail.com>
2024-11-16 12:07:56 -0600

    started implementing design patterns for lab four.

--- src/MakingChange.java
+++ src/MakingChange.java
@@ -5,15 +5,19 @@ import javax.swing.*;
 public class MakingChange {
     // creates JFrame
     public static void main(String[] args) {
+        // initialize a new frame
         JFrame frame = new JFrame();
-        // adds register panel to JFrame
+        // creates register panel for frame
         RegisterPanel registerPanel = new RegisterPanel(frame);
+        //adds frame
         frame.add(registerPanel);
+        // sets dimensions of frame
         frame.setPreferredSize(new Dimension(800,700));
-
+        // make gui close when you press the "X"
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+        // pack the frame
         frame.pack();
+        // set visible to true
         frame.setVisible(true);
 
     }
--- src/PursePanel.java
+++ src/PursePanel.java
@@ -9,6 +9,7 @@ public class PursePanel extends JPanel {
     private Purse purse;
 
     public final JLabel displayResetLabel = new JLabel("");
+
     public PursePanel() {
         this.purse = new Purse();
         this.setBackground(Color.WHITE);
@@ -19,20 +20,30 @@ public class PursePanel extends JPanel {
         this.purse = p;
 
     }
-
+    // this function is responsible for displaying images (also holds logic)
     public void setDisplayContents() {
-
+        // Outer loop will loop through the purse
         for (Map.Entry<DenominationList.Denominations, Integer> cash : purse.cash.entrySet()) {
+            // Inner loop will loop through the cash types in the purse (ex: dollars, quarters, hundreds etc)
             for(int i = 0; i < cash.getValue(); i++) {
+                // creates an image icon for a bill / coin. for example, if we have $5.23 this would load
+                // a $5 bill icon into the gui
                 ImageIcon icon = new ImageIcon(cash.getKey().img());
+                // gets image
                 Image img = icon.getImage();
+                // scales the image down so it can fit inside the gui window (this was a bug fix
+                // for me this may not apply to you)
                 Image scaledImg = img.getScaledInstance(75, 75, Image.SCALE_SMOOTH);
+                // add scaled image into the gui window (again this may not apply to you)
                 ImageIcon scaledIcon = new ImageIcon(scaledImg);
-
+                // create a label for the image icon (we need a label as a placeholder if we wanna add anything to the gui)
+                // if you wanna add to the gui, it needs a label
                 JLabel label = new JLabel(scaledIcon);
+                // add it to the gui
                 this.add(label);
             }
         }
+        // this is just resetting the gui window to be empty
         this.add(displayResetLabel);
         repaint();
 
